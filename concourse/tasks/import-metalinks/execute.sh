657c582efe3b9d0b6d5c95d6ac595ef954b8dbf3
Danny Berger <dpb587@gmail.com>
2017-07-02 20:16:28 -0700

    fix import

--- concourse/tasks/import-metalinks/execute.sh
+++ concourse/tasks/import-metalinks/execute.sh
@@ -2,13 +2,11 @@
 
 set -eu -o pipefail
 
-release_dir="$PWD/release"
-
 git clone --quiet "file://$PWD/repo" repo-output
 
 cd repo-output
 
-for compiled_metalink in $( find "$release_dir/compiled_releases" -name '*.meta4' ); do
+for compiled_metalink in $( find "data/$repository/compiled_releases" -name '*.meta4' ); do
   release_name="$( basename "$( dirname "$compiled_metalink" )" )"
   release_version="$( meta4 file-version --metalink "$compiled_metalink" )"
 
@@ -18,7 +16,7 @@ for compiled_metalink in $( find "$release_dir/compiled_releases" -name '*.meta4
   compiled_digest="$( meta4 file-hash --metalink "$compiled_metalink" sha-1 )"
   compiled_url="$( meta4 file-urls --metalink "$compiled_metalink" | head -n1 )"
 
-  source_metalink="$release_dir/releases/$release_name/$release_name-$release_version.meta4"
+  source_metalink="data/$repository/releases/$release_name/$release_name-$release_version.meta4"
   source_digest="$( meta4 file-hash --metalink "$source_metalink" sha-1 )"
 
   bcr-repo-file-add \
